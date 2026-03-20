91289d2dfbe8bd45f8a45081fc3056c7d5da2d91
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2026-03-20 14:21:46 +0000

    feat: Refactor GitHub roles and secrets management; unify organization variable usage and enhance configuration

---
+++ backend.hcl
@@ -0,0 +1,4 @@
+bucket  = "bootstrap-terraform-state-d9d6740f2fd-ca-central-1"
+key     = "bootstrap/terraform.tfstate"
+region  = "ca-central-1"
+encrypt = true
--- modules/github-secrets/main.tf
+++ modules/github-secrets/main.tf
@@ -1,14 +1,23 @@
 # GitHub Secrets and Variables Management
 # Sets up environment-level secrets and variables for repositories
 
-resource "github_actions_environment_secret" "repo_secrets" {
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+resource "github_actions_environment_secret" "env_secrets" {
   for_each = {
     for item in flatten([
       for repo in var.repositories : [
         for env in repo.environments : [
           for secret in env.secrets : {
-            key         = "${repo.owner}/${repo.name}/${env.name}/${secret.name}"
-            owner       = repo.owner
+            key         = "${var.github_org}/${repo.name}/${env.name}/${secret.name}"
+            owner       = var.github_org
             repo        = repo.name
             environment = env.name
             name        = secret.name
@@ -25,14 +34,14 @@ resource "github_actions_environment_secret" "repo_secrets" {
   plaintext_value = each.value.value
 }
 
-resource "github_actions_environment_variable" "repo_variables" {
+resource "github_actions_environment_variable" "env_variables" {
   for_each = {
     for item in flatten([
       for repo in var.repositories : [
         for env in repo.environments : [
           for variable in env.variables : {
-            key         = "${repo.owner}/${repo.name}/${env.name}/${variable.name}"
-            owner       = repo.owner
+            key         = "${var.github_org}/${repo.name}/${env.name}/${variable.name}"
+            owner       = var.github_org
             repo        = repo.name
             environment = env.name
             name        = variable.name
@@ -45,6 +54,46 @@ resource "github_actions_environment_variable" "repo_variables" {
 
   repository    = each.value.repo
   environment   = each.value.environment
+  variable_name = each.value.name
+  value         = each.value.value
+}
+
+resource "github_actions_secret" "repo_secrets" {
+  for_each = {
+    for item in flatten([
+      for repo in var.repositories : [
+        for secret in repo.secrets : {
+          key   = "${var.github_org}/${repo.name}/${secret.name}"
+          owner = var.github_org
+          repo  = repo.name
+          name  = secret.name
+          value = secret.value
+        }
+      ]
+    ]) : item.key => item
+  }
+
+  repository      = each.value.repo
+  secret_name     = each.value.name
+  plaintext_value = each.value.value
+}
+
+resource "github_actions_variable" "repo_variables" {
+  for_each = {
+    for item in flatten([
+      for repo in var.repositories : [
+        for variable in repo.variables : {
+          key   = "${var.github_org}/${repo.name}/${variable.name}"
+          owner = var.github_org
+          repo  = repo.name
+          name  = variable.name
+          value = variable.value
+        }
+      ]
+    ]) : item.key => item
+  }
+
+  repository    = each.value.repo
   variable_name = each.value.name
   value         = each.value.value
 }
